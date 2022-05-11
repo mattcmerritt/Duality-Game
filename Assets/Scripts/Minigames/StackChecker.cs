699afef210f185198c416e2847f2b2fbb5bb64f5
mikepmerritt <mikepmerritt@gmail.com>
2022-05-11 04:38:59 -0400

    added a stack grader

---
+++ Assets/Scripts/Minigames/StackChecker.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackChecker : MonoBehaviour
+{
+    private DraggableCan[] Cans;
+    private float CheckTimer = 0f;
+
+    private void Start()
+    {
+        Cans = FindObjectsOfType<DraggableCan>();
+    }
+
+    private void Update()
+    {
+        List<DraggableCan> top = new List<DraggableCan>();
+        List<DraggableCan> middle = new List<DraggableCan>();
+        List<DraggableCan> bottom = new List<DraggableCan>();
+
+        foreach (DraggableCan can in Cans)
+        {
+            if(can.transform.position.y > 3)
+            {
+                top.Add(can);
+            }
+            else if(can.transform.position.y > 0)
+            {
+                middle.Add(can);
+            }
+            else
+            {
+                bottom.Add(can);
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            CheckTimer = 1f;
+        }
+        else if (!Input.GetMouseButton(0))
+        {
+            if (CheckTimer >= 0f)
+            {
+                CheckTimer -= Time.deltaTime;
+            }
+            else if (top.Count >= 2 && middle.Count >= 3 && bottom.Count >= 4)
+            {
+                Debug.Log("Good job stacking the cans!");
+                // win stuff
+            }
+        }
+    }
+}
